4c1ae32b4f3691851514aa2ed1a50e7f41032284
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-01-08 08:09:41 -0500

    feat: include hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { HeroSection } from "@/components/home/hero-section";
+import { Header } from "@/components/layout/header";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">
+        <HeroSection />
+      </main>
     </div>
   );
-}
+};
