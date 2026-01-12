04c43bb879996f503730c1223f3c055c71fe33de
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-01-12 11:22:39 -0500

    feat: create and configure detailed page about movie

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,7 +1,12 @@
-export default function MoviePage() {
+import { tmdbServices } from "@/lib/services/tmdb";
+
+export default async function MoviePage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const data = await tmdbServices.getMovieById(Number(id));
+
   return (
     <div>
-      <h1>Movie Page</h1>
+      <h1>{data.title}</h1>
     </div>
   );
-}
+};
