4678b85cf80f831a5122a21b09d0c73a8dcfb73c
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-01-13 15:17:15 -0500

    feat: add new component to include the information about creation of movie

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { tmdbServices } from "@/lib/services/tmdb";
 import { MovieHero } from "@/components/movie/movie-hero";
+import { MovieInfo } from "@/components/movie/movie-info";
 
 export default async function MoviePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
@@ -9,6 +10,7 @@ export default async function MoviePage({ params }: { params: Promise<{ id: stri
   return (
     <div>
       <MovieHero movie={data} />
+      <MovieInfo movie={data} />
     </div>
   );
 };
