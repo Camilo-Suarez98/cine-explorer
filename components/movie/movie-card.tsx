0a97b0bf5e81b2a41e9060950d4241a0e97ce74c
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-01-24 19:57:08 -0500

    feat: show favorite button just for authenticated users

--- components/movie/movie-card.tsx
+++ components/movie/movie-card.tsx
@@ -6,9 +6,11 @@ import { Movie } from "@/lib/types/movie";
 import { getImageUrl } from "@/lib/utils/format";
 import { Heart, Star } from "lucide-react";
 import { useFavorites } from "@/context/favorites-context";
+import { useSession } from "next-auth/react";
 
 export const MovieCard = ({ movie }: { movie: Movie }) => {
   const { isFavorite, addFavorite, removeFavorite } = useFavorites();
+  const { status } = useSession();
   const favorite = isFavorite(movie.id);
 
   const toggleFavorite = (e: React.MouseEvent) => {
@@ -23,7 +25,7 @@ export const MovieCard = ({ movie }: { movie: Movie }) => {
 
   return (
     <Link href={`/movie/${movie.id}`} className="group relative">
-      <div className="flex flex-col gap-6 py-6 rounded-xl border shadow-sm transition-all hover:shadow-lg h-full">
+      <div className="flex flex-col gap-6 rounded-xl border shadow-sm transition-all hover:shadow-lg h-full">
         <div className="relative">
           <Image
             src={getImageUrl(movie.poster_path) || "/placeholder.svg"}
@@ -31,20 +33,22 @@ export const MovieCard = ({ movie }: { movie: Movie }) => {
             width={200}
             height={300}
             loading="eager"
-            className="w-full object-cover rounded-md md:h-[330px]"
+            className="w-full object-fill rounded-md md:h-[330px]"
           />
-          <button
-            onClick={toggleFavorite}
-            className="absolute top-2 right-2 z-10 p-2 cursor-pointer rounded-full bg-black/50 hover:bg-black/70 transition-colors"
-          >
-            <Heart
-              size={20}
-              className={favorite ? "fill-red-500 text-red-500" : "text-white"}
-            />
-          </button>
+          {status === "authenticated" && (
+            <button
+              onClick={toggleFavorite}
+              className="absolute top-2 right-2 z-10 p-2 cursor-pointer rounded-full bg-black/50 hover:bg-black/70 transition-colors"
+            >
+              <Heart
+                size={20}
+                className={favorite ? "fill-red-500 text-red-500" : "text-white"}
+              />
+            </button>
+          )}
           <div className="absolute inset-0 bg-linear-to-t from-black/60 via-transparent to-transparent opacity-0 transition-opacity group-hover:opacity-100 rounded-md"></div>
         </div>
-        <div className="p-4 flex flex-col flex-1">
+        <div className="px-4 pb-6 flex flex-col flex-1">
           <h2 className="font-semibold text-foreground line-clamp-1">{movie.title}</h2>
           <div className="mt-1 text-sm flex items-center justify-between">
             <p className="text-muted-foreground">{movie.release_date}</p>
